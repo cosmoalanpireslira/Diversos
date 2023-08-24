f0fc86792fb11257b5efdd42399fcbd80e225f36
Cosmo Alan Pires Lira <105260581+cosmoalanpireslira@users.noreply.github.com>
2023-08-24 19:34:44 -0300

    Add files via upload

---
+++ mario-jump/js/script.js
@@ -0,0 +1,9 @@
+const mario = documento.querySelector('.mario');
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(funcao, tempo);
+
+}
+
+return document.addEventListener('keyboard', jump);
